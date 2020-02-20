223cc81ede4820b06a0a71c268501a209bcb42f0
Marius Hagemann <marius@ing-hagemann.de>
2020-02-20 16:52:35 +0100

    tried to fix bug

--- networkit/cpp/randomization/BipartiteGlobalCurveball.cpp
+++ networkit/cpp/randomization/BipartiteGlobalCurveball.cpp
@@ -60,9 +60,7 @@ void BipartiteGlobalCurveball::make_trade(std::vector<node> &common, std::vector
 
     assert(nu + nv == disjoint.size());
 
-    std::array<node *, 2> results = {};
-
-    results = {disjoint_u.data(), disjoint_v.data()};
+    std::array<node *, 2> results =  {disjoint_u.data(), disjoint_v.data()};
 
     auto it = disjoint.cbegin();
     while (nu && nv) {
